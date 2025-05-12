29b6f0321fc2a0e67b047bb37b8f71e09846ac33
Aurimas L <kovas6@gmail.com>
2025-05-12 21:26:44 +0300

    postgres

---
+++ terraform/modules/postgres/main.tf
@@ -0,0 +1,130 @@
+locals {
+  postgres_username = getenv("POSTGRES_USER")
+  postgres_password = getenv("POSTGRES_PASSWORD")
+}
+
+resource "kubernetes_secret" "postgres" {
+  metadata {
+    name = "postgres-secret"
+  }
+
+  data = {
+    username = base64encode(local.postgres_username)
+    password = base64encode(local.postgres_password)
+  }
+
+  type = "Opaque"
+}
+
+resource "kubernetes_persistent_volume_claim" "postgres_pvc" {
+  metadata {
+    name = "postgres-pvc"
+  }
+
+  spec {
+    access_modes = ["ReadWriteOnce"]
+
+    resources {
+      requests = {
+        storage = "20Gi"
+      }
+    }
+
+    storage_class_name = "premium-rwo"
+  }
+}
+
+resource "kubernetes_deployment" "postgres" {
+  metadata {
+    name = "postgres"
+    labels = {
+      app = "postgres"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "postgres"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "postgres"
+        }
+      }
+
+      spec {
+        container {
+          name  = "postgres"
+          image = "postgres:15"
+
+          env {
+            name  = "POSTGRES_DB"
+            value = "appdb"
+          }
+
+          env {
+            name = "POSTGRES_USER"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.postgres.metadata[0].name
+                key  = "username"
+              }
+            }
+          }
+
+          env {
+            name = "POSTGRES_PASSWORD"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.postgres.metadata[0].name
+                key  = "password"
+              }
+            }
+          }
+
+          volume_mount {
+            name       = "postgres-storage"
+            mount_path = "/var/lib/postgresql/data"
+          }
+
+          port {
+            container_port = 5432
+          }
+        }
+
+        volume {
+          name = "postgres-storage"
+
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.postgres_pvc.metadata[0].name
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "postgres" {
+  metadata {
+    name = "postgres"
+  }
+
+  spec {
+    selector = {
+      app = "postgres"
+    }
+
+    port {
+      port        = 5432
+      target_port = 5432
+    }
+
+    type = "ClusterIP"
+  }
+}
