85f02471b8fccb52ef70660a699d8a9eceb4d07c
Aurimas L <kovas6@gmail.com>
2025-05-12 21:48:48 +0300

    fix provider

--- terraform/dev/provider.tf
+++ terraform/dev/provider.tf
@@ -1,6 +1,12 @@
 provider "google" {
   project = var.project_id
   region  = var.region
+  zone    = var.zone
+  # credentials is not needed as GCP credentials are provided via environment variable
 }
 
-data "google_client_config" "default" {}
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.cluster.endpoint}"
+  cluster_ca_certificate = base64decode(google_container_cluster.cluster.cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
+}
