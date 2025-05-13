a0e3932b9fe3de2c549afcfa37fc3723d92fba91
Aurimas L <kovas6@gmail.com>
2025-05-13 15:13:26 +0300

    ok

--- terraform/dev/provider.tf
+++ terraform/dev/provider.tf
@@ -2,7 +2,6 @@ provider "google" {
   project = var.project_id
   region  = var.region
   zone    = var.zone
-  # credentials picked up from environment (e.g., GitHub Secret GOOGLE_CREDENTIALS)
 }
 
 data "google_client_config" "default" {}
@@ -13,14 +12,14 @@ data "google_container_cluster" "primary" {
 }
 
 provider "kubernetes" {
-  host                   = "https://${data.google_container_cluster.primary.endpoint}"
+  host                   = data.google_container_cluster.primary.endpoint
   cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
   token                  = data.google_client_config.default.access_token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = "https://${data.google_container_cluster.primary.endpoint}"
+    host                   = data.google_container_cluster.primary.endpoint
     cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
     token                  = data.google_client_config.default.access_token
   }
