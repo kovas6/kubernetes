1e4ccc33c88a334e0eeb015ed476cd1528588403
Aurimas L <kovas6@gmail.com>
2025-05-13 11:55:50 +0300

    updated

--- terraform/dev/provider.tf
+++ terraform/dev/provider.tf
@@ -6,7 +6,17 @@ provider "google" {
 }
 
 provider "kubernetes" {
-  host                   = "https://${google_container_cluster.cluster.endpoint}"
-  cluster_ca_certificate = base64decode(google_container_cluster.cluster.cluster_ca_certificate)
+  host                   = google_container_cluster.primary.endpoint
+  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
   token                  = data.google_client_config.default.access_token
+}
+
+data "google_client_config" "default" {}
+
+provider "helm" {
+  kubernetes {
+    host                   = google_container_cluster.primary.endpoint
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+    token                  = data.google_client_config.default.access_token
+  }
 }
