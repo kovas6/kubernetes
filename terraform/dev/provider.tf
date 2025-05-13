d07efa138439824440d2a1dd43f6ed3dc2d43137
Aurimas L <kovas6@gmail.com>
2025-05-13 15:06:13 +0300

    fix

--- terraform/dev/provider.tf
+++ terraform/dev/provider.tf
@@ -2,21 +2,26 @@ provider "google" {
   project = var.project_id
   region  = var.region
   zone    = var.zone
-  # credentials is not needed as GCP credentials are provided via environment variable
-}
-
-provider "kubernetes" {
-  host                   = google_container_cluster.primary.endpoint
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
-  token                  = data.google_client_config.default.access_token
+  # credentials picked up from environment (e.g., GitHub Secret GOOGLE_CREDENTIALS)
 }
 
 data "google_client_config" "default" {}
 
+data "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region
+}
+
+provider "kubernetes" {
+  host                   = "https://${data.google_container_cluster.primary.endpoint}"
+  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  token                  = data.google_client_config.default.access_token
+}
+
 provider "helm" {
   kubernetes {
-    host                   = google_container_cluster.primary.endpoint
-    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+    host                   = "https://${data.google_container_cluster.primary.endpoint}"
+    cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
     token                  = data.google_client_config.default.access_token
   }
 }
