ea0d7255750f0f411fc2b2cb47c26b23cce8f3ce
Aurimas L <kovas6@gmail.com>
2025-05-14 22:16:28 +0300

    jjjj

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -5,10 +5,6 @@ terraform {
   }
 }
 
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.zone
