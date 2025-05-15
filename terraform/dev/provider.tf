0f8a10d6fb7c5b56e79f568d93b37745192d285f
Aurimas L <kovas6@gmail.com>
2025-05-15 11:21:55 +0300

    fffffffff

--- terraform/dev/provider.tf
+++ terraform/dev/provider.tf
@@ -1,16 +1,20 @@
+data "google_client_config" "provider" {}
+
 provider "google" {
   project = var.project_id
   region  = var.region
   zone    = var.zone
   # credentials not required since it's being passed through GITHUB secrets
 }
+
 provider "kubernetes" {
   host  = "https://${data.google_container_cluster.dev_cluster.endpoint}"
   token = data.google_client_config.provider.access_token
   cluster_ca_certificate = base64decode(
-    data.google_container_cluster.dev_cluster.master_auth[0].cluster_ca_certificate,
+    data.google_container_cluster.dev_cluster.master_auth[0].cluster_ca_certificate
   )
 }
+
 /*
 provider "kubernetes" {
   config_path = "~/.kube/config"
